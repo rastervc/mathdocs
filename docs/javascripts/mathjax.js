2461d14ede0a00486afc616d58154f080557aa16
rastervc <95301321+rastervc@users.noreply.github.com>
2025-11-29 21:14:15 +0200

    New beginnings

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,21 @@
+console.log('Done')
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
